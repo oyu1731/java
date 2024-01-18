162ae7627d932e7dcd96d020eeb875359d8241ed
oyu1731 <ktq2390303@o-hara.ac.jp>
2024-01-18 11:55:41 +0900

    commit

--- )8_8.java
+++ )8_8.java
@@ -2,20 +2,22 @@ package kyoutu;
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
 
 class Q8_8 {
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
+        int sum = 0;
         List<Integer> numbers = new ArrayList<Integer>();
+
         System.out.println("整数値を３つ入力");
-        for (int n = 1; n <= 3; n++) {
-            System.out.print(n + "つ目の整数値：");
+        for (int n = 0; n < 3; n++) {
+            System.out.print((n + 1) + "つ目の整数値：");
             int k = stdIn.nextInt();
             numbers.add(k);
-            
-        }
-        System.out.println("最大値" + numbers.stream().mapToInt(Integer::intValue).max());
-        System.out.println("最小値" + numbers.stream().mapToInt(Integer::intValue).min());
 
+        }
+        System.out.println("最大値" + Collections.max(numbers));
+        System.out.println("最小値" + Collections.min(numbers));
     }
 }
--- Q1_11.java
+++ Q1_11.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 class Q_11{
     public static void main(String[] args) {
-        Scanner stdIn = new Scanner(System.in)
+        Scanner stdIn = new Scanner(System.in);
     }
 }
