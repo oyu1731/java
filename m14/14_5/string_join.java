e97e90bd0c14fe6ee7198bb83f152d9e992a0949
oyu1731 <ktq2390303@o-hara.ac.jp>
2024-01-29 11:49:56 +0900

    c

--- m14/14_5/string_join.java
+++ m14/14_5/string_join.java
@@ -4,8 +4,15 @@ class stirng_join{
     public static void main(String[] args) {
      //------------------[ "Shuit-JIS"をつけることで文字化けを防ぐ ]----------------
      Scanner stdIn = new Scanner(System.in,"Shift-JIS");
+
+     System.out.print("１つ目のの文字列：");
      //-----------[ stringbuiderでインスタンス化 ]------------------------------
         //--------------[ これにより文字列をいろいろできる ]--------------
-        StringBuilder str =  new  StringBuilder(stdIn.next());
+        StringBuilder str_1 =  new  StringBuilder(stdIn.next());
+        System.out.print("２つ目の文字列：");
+        //--------------[ str_1 >> 先頭 str_2 >> 末尾に結合 ]--------------
+        str_1.append(stdIn.next());  
+
+        System.out.println(str_1);
     }
 }
